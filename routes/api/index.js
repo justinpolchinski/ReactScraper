096c4d7cec6672f1fbd30ad9e8609160f332501c
justinpolchinski <justinpolchinski@gmail.com>
2018-06-22 20:50:25 -0400

    routes change

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const articleRoutes = require("./article");
 
 // article routes
-router.use("/articles", articleRoutes);
+router.use("/article", articleRoutes);
 
 module.exports = router;
